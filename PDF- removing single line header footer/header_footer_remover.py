57811a307a491d047023fc00e34e793a86b3b3c1
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-10-31 07:56:36 +0000

    Add feature

---
+++ PDF- removing single line header footer/header_footer_remover.py
@@ -0,0 +1,32 @@
+
+"""
+!pip install bs4
+!pip install fuzzywuzzy
+!pip install tika 
+"""
+
+#Check if two strings match 
+def matcher(Str1,Str2):
+  from fuzzywuzzy import fuzz  #Fuzzywuzzy is modified version of 'levenshtein distance' to check similarity of strings
+  Partial_Ratio = fuzz.partial_ratio(Str1.lower(),Str2.lower())
+  Token_Sort_Ratio = fuzz.token_sort_ratio(Str1,Str2)
+  if (Token_Sort_Ratio>80) : 
+    return 1
+  else :
+    if Partial_Ratio>85 :  
+      return 1
+    else :
+      return 0
+
+import tika
+tika.initVM()
+from tika import parser
+parsed = parser.from_file('/content/brain.pdf', xmlContent=True)   #Input your PDF file here
+content= parsed["content"]
+#print(content)
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(content)
+divs = soup.findAll("div", { "class" : "page" })
+
+texts= {}             #stores header and footers
